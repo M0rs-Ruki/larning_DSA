6a11b88895418e2a4366381e8673197fc9857e34
Anup Pradhan <anuppradhan929@gmail.com>
2025-05-23 10:44:12 +0530

    Add C++ programs for various concepts including functions, binary conversion, bitwise operations, arrays, and vectors

---
+++ 9th_Lecture_Vectors.cpp
@@ -0,0 +1,111 @@
+
+// Vectors
+
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// int main() {
+
+//     // vector<int> vec;
+//     // vector<int> vec = {1,2,3,4,5};
+//     vector<int> vec(3,0);
+//     cout << vec.size() << endl;
+//     cout << vec[0] << endl;
+//     cout << vec[1] << endl;
+//     cout << vec[2] << endl;
+
+//     return 0;
+// }
+
+
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// int main() {
+//     // vector<int> vec(5,0);
+    
+//     // for(int i : vec) { // for each loop
+//     //     cout << i << endl;
+//     // }
+
+//     vector<char> vec = {'a','b','c','d','e'};
+    
+//     for(char i : vec) {
+//         cout << i << endl;
+//     }
+
+//     return 0;
+// }
+
+
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// int main() {
+
+//     vector<char> vecChar = {'a','b','c','d','e'};
+//     vector<int> vecInt = {1,2,3,4,5};
+
+//     cout << "(Char)size: " << vecChar.size() << endl;
+
+//     vecInt.push_back(99); // goint to push the element in the last 
+//     cout << "(Int)push_back: " << vecInt.size() << endl;
+
+//     vecInt.pop_back();
+//     cout << "(Int)pop_back: " << vecInt.size() << endl; // it's going to remove element from front
+
+//     cout << "(Int)front: " << vecInt.front() << endl; // goint to print the starting val
+//     cout << "(Int)back: " << vecInt.back() << endl; // goint to print the last val
+
+//     cout << "(Int)at: " << vecInt.at(2) << endl; // it work like the same as vecInt[2]
+
+
+//     for (int val : vecInt) {
+//         cout << val << endl;
+//     }
+    
+
+//     return 0;
+// }
+
+// static Vs Dynamic Allocation
+
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// int main() {
+//     vector<int> vec;
+//     vec.push_back(1);
+//     vec.push_back(2);
+//     vec.push_back(3);
+//     vec.push_back(4);
+//     vec.push_back(5);
+//     vec.push_back(6);
+
+//     cout << vec.size() << endl;
+//     cout << vec.capacity() << endl;
+
+//     return 0;
+// }
+
+
+// Problem 
+
+// single Number;
+
+
+
+// problem
+// Linear search on vector
+
+
+
+
+// Reverse code on vector
+
+
+
