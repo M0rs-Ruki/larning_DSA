693d2ccb386d2589bcb60c0d0e4ac0da5898c528
Anup Pradhan <anuppradhan929@gmail.com>
2025-05-28 20:33:43 +0530

    Add C++ implementation for Power Function (myPow) and update formatting in Dsa.cpp

--- Dsa.cpp
+++ Dsa.cpp
@@ -66,7 +66,7 @@ using namespace std;
 
 
 
-//                            35. Search Insert Position
+//                                                        35. Search Insert Position
 
 // class Solution {
 // public:
@@ -168,3 +168,85 @@ using namespace std;
 //         return ans;
 //     }
 // };
+
+
+
+//                                                                  50. Pow(x, n)
+
+// class Solution {
+// public:
+//     double myPow(double x, int n) {
+        
+//         if(n == 0) return 1.0;
+//         if(x == 0) return 0.0;
+//         if(x == 1) return 1.0;
+//         if(x == -1 && n%2 == 0) return 1.0;
+//         if(x == -1 && n%2 != 0) return -1.0;
+
+//         long binForm = n;
+
+//         if(binForm < 0) {
+//             x = 1/x;
+//             binForm = -binForm;
+//         }
+
+//         double ans = 1;
+
+//         while (binForm > 0)
+//         {
+//             if(binForm % 2 == 1) {
+//                 ans *=x;
+//             }
+//             x *= x;
+//             binForm /= 2;
+//         }
+//         return ans;
+//     }
+// };
+
+
+// #include <iostream>
+// using namespace std;
+
+// double myPow(double x, int n) {
+
+//     if(n == 0) return 1.0;
+//     if(x == 0) return 0.0;
+//     if(x == 1) return 1.0;
+//     if(x == -1 && n%2 == 0) return 1.0;
+//     if(x == -1 && n%2 != 0) return -1.0;
+
+//     long bin = n;
+
+//     if(bin < 0) {
+//         x = 1/x;
+//         bin = -bin;
+//     }
+    
+//     double ans = 1;
+
+//     while (bin > 0)
+//     {
+//         if(bin % 2 == 1) {
+//             ans *= x;
+//         }
+//         x *= x;
+//         bin /= 2;
+//     }
+//     return ans;
+// }
+
+// int main() {
+//     double x = 2;
+//     int n = 5;
+
+//     double result = myPow(x, n);
+//     cout << "2^5 = " << result << endl;  // Output: 32
+//     return 0;
+// }
+
+
+
+//                                                  121. Best Time to Buy and Sell Stock
+
+
