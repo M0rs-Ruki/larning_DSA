e999b94db6914cee263a6c21eeef14c874928ef6
Anup Pradhan <anuppradhan929@gmail.com>
2025-06-03 18:36:48 +0530

    33. Search in Rotated Sorted Array

--- Dsa.cpp
+++ Dsa.cpp
@@ -422,9 +422,9 @@ using namespace std;
 //             // int mid = (start + end)/ 2;
 //             int mid = start + (end - start) / 2 ;
 //             if(target > nums[mid]) {
-//                 start = mid+1;
+//                 start = mid+1; // right
 //             }else if (target < nums[mid]){
-//                 end = mid-1;
+//                 end = mid-1; // left
 //             }else {
 //                 return mid;
 //             }
@@ -433,3 +433,41 @@ using namespace std;
 //         return -1;
 //     }
 // };
+
+
+//                                               33. Search in Rotated Sorted Array
+
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+        int n = nums.size();
+        int st = 0;
+        int end = n-1;
+        
+        while (st <= end){
+
+            int mid = st +(end - st) / 2;
+            
+            if(nums[mid] == target) {
+                return mid;
+            }
+            
+            if(nums[st] <= nums[mid]){ // left shorted
+                if(nums[st] <= target &&  target <= nums[mid]){
+                    end = mid-1;
+                } else { 
+                    st = mid+1;
+                }
+            } else {  // right shorted
+                if(nums[mid] <= target && target <= nums[end]){
+                    st = mid+1;
+                } else {
+                    end = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+};
