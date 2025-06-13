939d094949bdcee5a71382acce2f56108e2c5cbc
Anup Pradhan <anuppradhan929@gmail.com>
2025-06-13 08:32:27 +0530

    Cstrings

---
+++ Strings/Cstrings.cpp
@@ -0,0 +1,40 @@
+
+
+// strings in c++
+// Anything in " " is a string
+
+// caracter arrays => Cstrings
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <string>
+using namespace std;
+
+// int main() {
+//     // char str[] = {'a','b','c','\0'};
+//     char str[] = "abc";
+//     cout << str << endl; // constant Pointers
+//     // cout << strlen(str) << endl;
+//     return 0;
+// }
+
+// int main() {
+//     char str[100];
+//     cout << "Enter Somthing : " ;
+//     cin >> str;
+//     cin.getline(str, 100);
+//     cout << str << endl;
+//     return 0;
+// }
+
+// int main() {
+//     char str[] = "Hello Mors!!";
+//     int len =0;
+//     for(int i= 0; str[i] != '\0'; i++) {
+//         len++;
+//     }
+//     cout << len << endl;
+//     return 0;
+// }
