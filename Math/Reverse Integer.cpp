2e31eb023daef31db36d2db88b2c6abbba195b0d
Anup Pradhan <anuppradhan929@gmail.com>
2025-06-19 23:53:34 +0530

    Fix integer reversal logic to correctly handle underflow condition

--- Math/Reverse Integer.cpp
+++ Math/Reverse Integer.cpp
@@ -5,7 +5,7 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <cmath>
+#include <climits>
 using namespace std;
 
 
@@ -30,7 +30,7 @@ public:
         int rev = 0;
         while (x != 0) {
             int pop = x % 10;
-            if(rev > INT_MAX/10 || (rev == INT_MIN / 10) ) {
+            if(rev > INT_MAX/10 || (rev < INT_MIN / 10) ) {
                 return 0;
             }
             rev *= 10;
