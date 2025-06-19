82fd15f8d01d0faeac1a03ca2728a16295893ec0
Anup Pradhan <anuppradhan929@gmail.com>
2025-06-19 23:53:27 +0530

    Add implementation for reversing an integer and update notes with relevant section

---
+++ Math/Reverse Integer.cpp
@@ -0,0 +1,42 @@
+
+
+// 7. Reverse Integer
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+
+// class Solution {
+// public:
+//     int reverse(int x) {
+//         int rev = 0;
+//         while (x != 0) {
+//             int pop = x % 10;
+//             rev *= 10;
+//             rev += pop;
+//             x /= 10;
+//         }
+//         return rev;
+//     }
+// };
+
+
+class Solution {
+public:
+    int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            if(rev > INT_MAX/10 || (rev == INT_MIN / 10) ) {
+                return 0;
+            }
+            rev *= 10;
+            rev += pop;
+            x /= 10;
+        }
+        return rev;
+    }
+};
