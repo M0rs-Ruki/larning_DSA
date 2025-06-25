512761e02e28e4cd651c1d6d86f7ef860db7b316
Anup Pradhan <anuppradhan929@gmail.com>
2025-06-25 12:27:57 +0530

    Add functions to calculate maximum row and column sums in a 2D array

--- 2D-Arrays/Maximum Row Sum.cpp
+++ 2D-Arrays/Maximum Row Sum.cpp
@@ -1,2 +1,61 @@
 
+// Maximum Row Sum
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <climits>
+using namespace std;
 
+pair<int, int> findMaxRowSum(vector<vector<int>>& mat) {
+    int maxSum = INT_MIN;
+    int rowIndex = -1;
+
+    for (int i = 0; i < mat.size(); i++) {
+        int sum = 0;
+        for (int j = 0; j < mat[i].size(); j++) {
+            sum += mat[i][j];
+        }
+
+        if (sum > maxSum) {
+            maxSum = sum;
+            rowIndex = i;
+        }
+    }
+
+    return {rowIndex, maxSum};
+}
+
+
+pair<int, int> findMaxColumSum(vector<vector<int>>& mat) {
+    int maxSum = INT_MIN;
+    int columIndex = -1;
+
+    for (int col = 0; col < mat[0].size(); col++) {
+        int sum = 0;
+        for (int row = 0; row < mat.size(); row++) {
+            sum += mat[row][col];
+        }
+
+        if (sum > maxSum) {
+            maxSum = sum;
+            columIndex = col;
+        }
+    }
+
+    return {columIndex, maxSum};
+}
+
+
+int main() {
+    vector<vector<int>> matrix = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    auto result = findMaxRowSum(matrix);
+    cout << "Row with max sum: " << result.first << ", Sum: " << result.second << endl;
+    auto resultCo = findMaxColumSum(matrix);
+    cout << "Column with max sum: " << resultCo.first << ", Sum: " << resultCo.second << endl;
+
+}
