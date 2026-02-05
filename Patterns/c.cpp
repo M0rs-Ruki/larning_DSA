bd5368e3ed012031ccc108c50687c5e462a1bfdd
Anup Pradhan <anuppradhan929@gmail.com>
2026-02-06 00:45:08 +0530

    feat: Create c.cpp with C++ code for printing numerical patterns.

---
+++ Patterns/c.cpp
@@ -0,0 +1,52 @@
+
+#include <iostream>
+using namespace std; 
+
+// int main() {
+//         int n = 4;
+//         for (int i = 1; i <= n; i++) {
+//             for (int j = 1; j <= n; j++) {
+//                 cout << j;
+//             }
+//             cout << endl;
+//         }   
+//     return 0;
+// }
+// 1234
+// 1234
+// 1234
+// 1234 a loop that print 1234 four times nice 
+
+
+// int main() {
+//     int n = 4;
+//     for (int i = 1; i <=n; i++) {
+//         cout << i;
+//     }
+//     return 0;
+// } // 1234    a normal loop
+
+
+// int main() {
+//     for (int i = 0; i < 4; i++) {
+//         char ch = 'A';
+//         for (int j = 0; j < 4; j++) {
+//             cout << ch;
+//             ch++;
+//         };
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
+int main() { 
+    int in = 1;
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; in < 10; j++) {
+            cout << in;
+            in++;
+        };
+        cout << endl;
+    }
+    return 0;
+}
