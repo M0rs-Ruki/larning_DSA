0335d58eb4b67dc8bb07501fce9383a5828b6a6e
Anup Pradhan <anuppradhan929@gmail.com>
2026-02-14 00:21:12 +0530

    Refactor `c.cpp` to implement a new pattern printing logic, replacing previous examples with a pyramid-like structure and ensuring proper formatting and spacing in output.

--- Patterns/c.cpp
+++ Patterns/c.cpp
@@ -81,12 +81,26 @@ using namespace std;
 //     }
 // }
 
+// int main() {
+//     int n = 4;
+//     for (int i = 0; i < n; i++) {
+//         for (int j =i+1; j>0; j--) {
+//             cout << j ;
+//         }
+//         cout << endl;
+//     }
+// }
+
 int main() {
     int n = 4;
-    for (int i = 0; i < n; i++) {
-        for (int j =i+1; j>0; j--) {
-            cout << j ;
+    for (int i=0; i<=n; i++) {
+        for (int j=0; j<i; j++) {
+            cout << " ";
+        }
+        for (int j=0; j<n-i; j++) {
+            cout << (i+1);
         }
         cout << endl;
     }
+    return 0;
 }
